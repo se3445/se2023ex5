ae34610570e2e39880174b018a5983e691677385
se3445 <r202203445.cb@jindai.jp>
2024-01-31 18:38:00 +0900

    count character

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
   char str[] = "hello";
-  printf("%s", str);
+  printf("文字列: %s\n", str);
+  printf("長さ: %lu\n", strlen(str));
   return 0;
 }
